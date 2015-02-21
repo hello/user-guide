5df0ee69c4627571857bd8eaa512084e423e945e
longd3 <izzy3ds@gmail.com>
2015-02-21 08:11:35 +0700

    If invoking hugo throws error/critical, stop uploading to s3

--- cibuild.sh
+++ cibuild.sh
@@ -11,26 +11,30 @@ unzip awscli-bundle.zip
 
 #!/bin/bash
 if [ $TRAVIS_BRANCH == 'staging' ]; then
-	# clear all existing content in s3 bucket
+	echo "Clear all existing content in s3 bucket"
 	/home/travis/bin/aws s3 rm s3://staging-user.hello.is/ --recursive
 	
 	echo
 	head -1 config.toml
 	echo
 
-	# invoke hugo to build /public
-	./hugo-ci
+	echo "Invoke hugo to build /public and log"
+	./hugo-ci --logFile="hugo-log"
 
-	#copy 404 page to error page
-	cp public/404.html public/error.html
+	if [ grep -q ERROR "hugo-log" ] || [ grep -q CRITICAL "hugo-log" ]; then 
+		echo "\n Hugo has failed to generate content \n"
+	else
+		echo "copy 404 page to error page"
+		cp public/404.html public/error.html
 
-	# run awscli s3 command to upload /public to staging bucket for internal testing
-	echo "Uploading to staging s3"
-	/home/travis/bin/aws s3 cp public/ s3://staging-user.hello.is/ --recursive
+		echo "run awscli s3 command to upload /public to staging bucket for internal testing"
+		echo "Uploading to staging s3"
+		/home/travis/bin/aws s3 cp public/ s3://staging-user.hello.is/ --recursive
+	fi
 fi
 
 if [ $TRAVIS_BRANCH == 'master' ]; then
-	# Update master base url
+	echo "Update master base url"
 	mv config.toml config-staging.toml
 	sed 's/http:\/\/staging-user.hello.is/http:\/\/guide.hello.is/g' config-staging.toml > config.toml
 	find . -iregex '.*\(toml\)'
@@ -39,14 +43,17 @@ if [ $TRAVIS_BRANCH == 'master' ]; then
 	head -1 config.toml
 	echo
 	
-	# invoke hugo to build /public
-	./hugo-ci
+	echo "Invoke hugo to build /public and log"
+	./hugo-ci --logFile="hugo-log"
 
-	#copy 404 page to error page
-	cp public/404.html public/error.html
+	if [ grep -q ERROR "hugo-log" ] || [ grep -q CRITICAL "hugo-log" ]; then 
+		echo "\n Hugo has failed to generate content \n"
+	else
+		echo "copy 404 page to error page"
+		cp public/404.html public/error.html
 
-	# run awscli s3 command to upload /public to master bucket to have it published
-	# TODO: update bucket name correctly here
-	echo "Uploading to master s3"
-	/home/travis/bin/aws s3 cp public/ s3://staging-user.hello.is/ --recursive
+		echo "run awscli s3 command to upload /public to master bucket to have it published"
+		echo "Uploading to master s3"
+		/home/travis/bin/aws s3 cp public/ s3://staging-user.hello.is/ --recursive
+	fi
 fi
